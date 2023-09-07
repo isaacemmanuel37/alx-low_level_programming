037238f12229d3901c4d3db46e42a0d6c4d686d0
isaacemmanuel37 <isaacochukoemmanuel@gmail.com>
2023-09-07 10:30:46 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - this is the main function
- *
  * Return: always 0
  */
 int main(void)
 {
 puts("\"programming is like a multilingual puzzle");
 return (0);
-}			
+}	
