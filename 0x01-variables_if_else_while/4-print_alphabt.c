2e3af918d8b508542d2fe463928959d005b99439
isaacemmanuel37 <isaacochukoemmanuel@gmail.com>
2023-09-08 08:31:48 -0700

    alphabet 2

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 when sucessful
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	{
+	if (letter != 'e' && letter != 'q')
+	putchar(letter);
+	}
+
+	putchar('\n');
+	return (0);
+}
