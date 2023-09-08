ac1a0b4b8f7510418f3902ba83f28315e152a3d3
isaacemmanuel37 <isaacochukoemmanuel@gmail.com>
2023-09-08 13:16:09 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - main function
+ * Description: prints char below 10
  * Return: 0 when sucessful
  */
 
 int main(void)
 {
-	char letter;
-	for (l
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+	putchar(a + '0');
+	}
+	putchar('\n');
+	return (0);
+}
