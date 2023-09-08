8780e5578bae4449d21ed9f3a95e8fa8de6e1b7a
isaacemmanuel37 <isaacochukoemmanuel@gmail.com>
2023-09-08 14:04:00 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0 when sucessful
+ */
+
+int main(void)
+{
+int num;
+char letter;
+
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+
+for (letter = 'a'; letter <= 'f' ; letter++)
+
+putchar('\n');
+return (0);
+
+}
