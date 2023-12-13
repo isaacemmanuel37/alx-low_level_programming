a208e77ccfb7b327973f030fad5d0386f9bb7ea2
isaacemmanuel37 <isaacochukoemmanuel@gmail.com>
2023-12-12 16:10:00 -0800

    alx

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+/**
+ * mul - mul two integers
+ * @a: first int
+ * @b: second int
+ * Return: multiply
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+/**
+ * sub - subtract two integers
+ * @a: first int
+ * @b: second int
+ * Return: subtraction
+ */
+int sub(int a, int b)
+{
+	return(a - b);
+}
